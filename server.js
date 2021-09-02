fe4989e0e049c9a01ce4f7577aa1fa534207ef98
Bright Amoateng <bright.amoateng150@gmail.com>
2021-09-02 00:57:46 -0400

    Create web server and pages

--- server.js
+++ server.js
@@ -1,1 +1,25 @@
-console.log("Hello world!");
+require('dotenv').config();
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
+
+const { PORT } = process.env;
+const app = express();
+const salt = 10;
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static("public"));
+
+
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.listen(PORT, () => {
+    console.log(`Running on port ${PORT}`);
+});

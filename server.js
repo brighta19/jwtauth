87161ba7df2b3e6aee8778a263493ee01c053c1f
Bright Amoateng <bright.amoateng150@gmail.com>
2021-09-08 13:59:35 -0400

    Create a user

--- server.js
+++ server.js
@@ -1,23 +1,49 @@
-require('dotenv').config();
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
+require("dotenv").config();
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-const { PORT } = process.env;
+const DUPLICATE_ERROR_CODE = 11000;
+const { PORT, JWT_SECRET, MONGODB_URL } = process.env;
 const app = express();
 const salt = 10;
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.static("public"));
 
+mongoose.connect(MONGODB_URL, {useNewUrlParser: true, useUnifiedTopology: true});
 
-app.get('/',(req,res)=>{
-    res.render('home');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }
+}, { collection: "users" });
+const User = mongoose.model("User", userSchema);
+
+app.post("/signup", async (req, res) => {
+    const { email, password: plainTextpassword } = req.body;
+    const password = await bcrypt.hash(plainTextpassword, salt);
+
+    try {
+        await User.create({ email, password });
+        return res.redirect("/");
+    }
+    catch (error) {
+        console.log(JSON.stringify(error));
+        if (error.code === DUPLICATE_ERROR_CODE) {
+            return res.send({ status: "error", error: "Email already exists" });
+        }
+        throw error;
+    }
+});
+
+app.get("/",(req, res)=>{
+    res.render("home");
 })
 
 app.listen(PORT, () => {
